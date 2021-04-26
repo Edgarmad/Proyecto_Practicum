6feaae0bc9585648aa3628f0a136ee9c99340094
Edgarmad <edmad1396@gmail.com>
2021-04-26 16:51:17 -0500

    prueba de database

--- buscar.php
+++ buscar.php
@@ -1,20 +1,15 @@
 <?php
-$dbhost = 'localhost';
-$dbname = 'sistema_practicum';
-$dbuser = 'root';
-$dbpass = '';
-$connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-      if($connection->connect_error){
-        die("Conexión fallida: ".$connection->connect_error);
-      }
+include("db.php");
 
     $salida = "";
 
-    $query = "SELECT * FROM datos_personales WHERE nombre NOT LIKE '' ORDER By id LIMIT 25";
+    $query = "SELECT * FROM ofertas_trabajo WHERE nombre_vacante  NOT LIKE '' ORDER By id LIMIT 25";
 
     if (isset($_POST['consulta'])) {
     	$q = $connection->real_escape_string($_POST['consulta']);
-    	$query = "SELECT * FROM datos_personales WHERE id LIKE '%$q%' OR nombre LIKE '%$q%' OR apellido LIKE '%$q%' OR email LIKE '%$q%' OR telefono LIKE '$q' ";
+    	$query = "SELECT * FROM ofertas_trabajo WHERE nombre_vacante  LIKE '%$q%' OR nombre_coordinador LIKE '%$q%' 
+																	 OR nombre_empresa LIKE '%$q%' OR skills_necesitadas LIKE '$q' 
+																	 OR skills_desarrollar LIKE '%$q%'";
     }
 
     $resultado = $connection->query($query);
@@ -23,11 +18,11 @@ $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
     	$salida.="<table border=1 class='tabla_datos'>
     			<thead>
     				<tr id='titulo'>
-    					<td>ID</td>
-    					<td>Nombre</td>
-    					<td>Apellido</td>
-    					<td>Correo</td>
-    					<td>Telefono</td>
+						<td>Vacante</td>
+    					<td>Coordinador de proyecto</td>
+    					<td>Nombre de la empresa</td>
+    					<td>Habilidades necesarias</td>
+    					<td>Habilidades a desarrollar</td>
     				</tr>
 
     			</thead>
@@ -37,17 +32,17 @@ $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
     	while ($fila = $resultado->fetch_assoc()) {
     		$salida.="<tr>
-    					<td>".$fila['id']."</td>
-    					<td>".$fila['nombre']."</td>
-    					<td>".$fila['apellido']."</td>
-    					<td>".$fila['email']."</td>
-    					<td>".$fila['telefono']."</td>
+    					<td>".$fila['nombre_vacante']."</td>
+    					<td>".$fila['nombre_coordinador']."</td>
+    					<td>".$fila['nombre_empresa']."</td>
+    					<td>".$fila['skills_necesitadas']."</td>
+    					<td>".$fila['skills_desarrollar']."</td>
     				</tr>";
 
     	}
     	$salida.="</tbody></table>";
     }else{
-    	$salida.="NO HAY DATOS :(";
+    	$salida.="Sin resultados";
     }
 
 
--- db.php
+++ db.php
@@ -1,8 +1,12 @@
 <?php
 session_start();
-$dbhost = 'localhost';
+$dbhost = 'database-1.claawir8rmgn.us-east-2.rds.amazonaws.com';
 $dbname = 'sistema_practicum';
-$dbuser = 'root';
-$dbpass = '';
-$connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+$dbuser = 'admin';
+$dbpass = 'sistemapracticum';
+$port = '3306';
+$connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname,$port);
+if($connection->connect_error){
+    die("Conexión fallida: ".$connection->connect_error);
+  }
 ?>
